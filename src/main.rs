c20976856bcf972475037181dcda60f07551af34
Leo Savi <lsavi@fullstacklabs.co>
2025-05-18 18:58:02 -0300

    feat(main): use cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::env;
+
 use chipo::emulator::emulator::Emulator;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Expected 1 argument, got {} instead.", args.len() - 1);
+    }
+
     let mut emulator = Emulator::new();
-    let rom_path: &str = "roms/pong";
-    emulator.load_rom(rom_path).unwrap_or_else(|err| {
+    emulator.load_rom(&args[1]).unwrap_or_else(|err| {
         println!("Cannot open rom! {}", err);
     });
 
